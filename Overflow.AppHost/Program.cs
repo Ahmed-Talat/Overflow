2febdf79e7573250fd68e768b3645e54a969a9c3
Ahmed Talaat <talat.salem@gmail.com>
2025-10-18 14:59:14 +0300

    Add Questions Servies and Postgres

--- Overflow.AppHost/Program.cs
+++ Overflow.AppHost/Program.cs
@@ -3,4 +3,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithDataVolume("keycloak.data");
 
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres.data")
+    .WithPgAdmin();
+
+var postgresDb = postgres.AddDatabase("questionDb");
+
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WithReference(postgresDb)
+    .WaitFor(keycloak)
+    .WaitFor(postgresDb);
+
 builder.Build().Run();
