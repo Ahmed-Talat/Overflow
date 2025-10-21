c1b9008983ccbb42a31a66ee77f11cf147cc33c6
Ahmed Talaat <talat.salem@gmail.com>
2025-10-21 20:17:43 +0300

    Add Search Service

--- Overflow.AppHost/Program.cs
+++ Overflow.AppHost/Program.cs
@@ -9,10 +9,33 @@ var postgres = builder.AddPostgres("postgres", port: 5432)
 
 var postgresDb = postgres.AddDatabase("questionDb");
 
+
+var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey, "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithHttpEndpoint(8108, 8108, name: "typesense");
+
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
+var rabbitMq = builder.AddRabbitMQ("messaging")
+    .WithDataVolume("messaging.data")
+    .WithManagementPlugin(port: 15672);
+
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
     .WithReference(keycloak)
     .WithReference(postgresDb)
+    .WithReference(rabbitMq)
     .WaitFor(keycloak)
-    .WaitFor(postgresDb);
+    .WaitFor(postgresDb)
+    .WaitFor(rabbitMq);
+
+var searchService = builder.AddProject<Projects.SearchService>("search-svc")
+    .WithEnvironment("typesense-api-key", typesenseApiKey)
+    .WithReference(typesenseContainer)
+    .WithReference(rabbitMq)
+    .WaitFor(typesense)
+    .WaitFor(rabbitMq);
 
 builder.Build().Run();
